0e9c35dd394eed30bacd53bbfe4d2161381d068b
Tybern82 <jeffpublic@kintoshmalae.com>
2021-06-05 19:20:26 +1000

    Tidy / Comment

--- MultiChatServerv1/ChatServerSettings.cs
+++ MultiChatServerv1/ChatServerSettings.cs
@@ -16,7 +16,11 @@ namespace MultiChatServer {
     /// </summary>
     public class ChatServerSettings {
 
-        public ChatServerSettings(JToken jsonData) {
+        /// <summary>
+        /// Load settings from the given JSON data
+        /// </summary>
+        /// <param name="jsonData">data object containing settings to load</param>
+        public ChatServerSettings(JToken jsonData) : this() {
             string? curr = jsonData.Value<string>("brimeName");
             BrimeName = (curr != null) ? curr : "";
             curr = jsonData.Value<string>("brimeChannelID");
@@ -35,16 +39,12 @@ namespace MultiChatServer {
                     curr = item.Value<string>();
                     if (curr != null) IgnoreChatNames.Add(curr);
                 }
-            } else {
-                IgnoreChatNames = new List<string>();
-                // setup some defaults
-                IgnoreChatNames.AddRange(new string[] {
-                    // Add some recommended defaults
-                    "streamelements", "streamlabs", "nightbot", "commanderroot", "soundalerts", "pretzelrocks", "restreambot"
-                });
             }
         }
 
+        /// <summary>
+        /// Create a default set of settings.
+        /// </summary>
         public ChatServerSettings() {
             BrimeName = "";
             BrimeChannelID = "";
@@ -60,13 +60,39 @@ namespace MultiChatServer {
                 });
         }
 
+        /// <summary>
+        /// Login name to connect to on Brime (will trigger Channel ID lookup once Client-ID available)
+        /// </summary>
         public string BrimeName { get; set; }
+
+        /// <summary>
+        /// Channel ID to connect to on Brime (should be preferred once new chat system implemented)
+        /// </summary>
         public string BrimeChannelID { get; set; }
+
+        /// <summary>
+        /// Identify whether to connect to Twitch stream
+        /// </summary>
         public bool ConnectTwitch { get; set; }
+
+        /// <summary>
+        /// Identify whether to connect to Trovo stream
+        /// </summary>
         public bool ConnectTrovo { get; set; }
+
+        /// <summary>
+        /// Identify whether to connect to YouTube stream
+        /// </summary>
         public bool ConnectYouTube { get; set; }
+
+        /// <summary>
+        /// Identify whether to show the log window
+        /// </summary>
         public bool ShowLog { get; set; }
 
+        /// <summary>
+        /// List of names to ignore as "bot" accounts when counting Viewer numbers (Twitch only)
+        /// </summary>
         public List<string> IgnoreChatNames { get; private set; }
 
 
@@ -79,6 +105,11 @@ namespace MultiChatServer {
             "    \"showLog\": {5}, \n" +
             "    \"ignoreNames\": {6} \n" +
             "}}";
+
+        /// <summary>
+        /// Convert settings back to JSON object to use when saving to settings file.
+        /// </summary>
+        /// <returns>JSON entity containing curent settings</returns>
         public string ToJSON() {
             return string.Format(JSON_FORMAT,
                 BrimeName.ToJSONString(),
